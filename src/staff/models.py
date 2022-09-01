00c529d9def5b26776f062deeed4ed7bba223ec3
Thomas Roussel <thomas.roussel@ik.me>
2022-09-01 17:09:23 +0200

    email as username (manager & admin)

--- src/staff/models.py
+++ src/staff/models.py
@@ -1,5 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('An email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     ROLE_CHOICES = [
@@ -14,6 +38,8 @@ class User(AbstractUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
+    objects = UserManager()
 
     def __str__(self):
         return self.email
