9930556efdd4af078f050c37eee1a8ac119630b1
Thomas Roussel <thomas.roussel@ik.me>
2022-09-09 16:51:44 +0200

    views and obj perms

---
+++ src/clients/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class ClientAccessPermission(BasePermission):
+    message = 'You are not assigned to this client'
+    
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+    
+    def has_object_permission(self, request, view, obj):
+        return True if request.method in SAFE_METHODS else obj.sales_contact == request.user
