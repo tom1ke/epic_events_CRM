6d05982f9b32ed5dba03639af7caf821d412b91c
Thomas Roussel <thomas.roussel@ik.me>
2022-09-14 18:28:27 +0200

    updated perms

--- src/contracts/permissions.py
+++ src/contracts/permissions.py
@@ -2,10 +2,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class ContractAccessPermission(BasePermission):
-    message = 'You are not assigned to this contract'
     
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
+        return True if request.method in SAFE_METHODS else request.user.role == 2
     
     def has_object_permission(self, request, view, obj):
         return True if request.method in SAFE_METHODS else obj.sales_contact == request.user
--- src/events/permissions.py
+++ src/events/permissions.py
@@ -2,10 +2,23 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class EventAccessPermission(BasePermission):
-    message = 'You are not assigned to this event'
     
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
-    
+        if request.method in SAFE_METHODS:
+            return True
+        
+        if request.user.role == 2:
+            return True
+        
+        if request.method != 'POST' and request.user.role == 3:
+            return True
+        
     def has_object_permission(self, request, view, obj):
-        return True if request.method in SAFE_METHODS else obj.support_contact == request.user
+        if request.method in SAFE_METHODS:
+            return True
+        
+        if obj.support_contact == request.user:
+            return True
+        
+        if obj.client.sales_contact == request.user:
+            return True
