e311e6f64489b8f19e8526a11d105c27bc761922
Thomas Roussel <thomas.roussel@ik.me>
2022-09-14 19:17:34 +0200

    fixed create mixin

--- src/events/views.py
+++ src/events/views.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Event
@@ -5,7 +6,28 @@ from .serializers import EventSerializer
 from .permissions import EventAccessPermission
 
 
-class AllEventsViewSet(ModelViewSet):
+class CreateEventMixin:
+    
+    def create(self, request, *args, **kwargs):
+        event_data = request.data
+        
+        new_event = Event.objects.create(
+            client_id = event_data['client'],
+            attendees = event_data['attendees'],
+            event_date = event_data['event_date'],
+            notes = event_data['notes'],
+            support_contact = self.request.user,
+            status_id = 1
+        )
+        
+        new_event.save()
+        
+        serializer = EventSerializer(new_event)
+        
+        return Response(serializer.data)
+
+
+class AllEventsViewSet(CreateEventMixin, ModelViewSet):
     
     serializer_class = EventSerializer
     permission_classes = [EventAccessPermission]
@@ -14,7 +36,7 @@ class AllEventsViewSet(ModelViewSet):
         return Event.objects.all()
 
 
-class EventViewSet(ModelViewSet):
+class EventViewSet(CreateEventMixin, ModelViewSet):
     
     serializer_class = EventSerializer
     permission_classes = [EventAccessPermission]
