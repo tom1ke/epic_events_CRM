cb5bcefe9cbffc5239f9fb1d2bf7a2255794d8bc
Thomas Roussel <thomas.roussel@ik.me>
2022-09-14 17:37:49 +0200

    view only

--- src/staff/permissions.py
+++ src/staff/permissions.py
@@ -4,7 +4,5 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class UserAccessPermission(BasePermission):
     
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
-    
-    def has_object_permission(self, request, view, obj):
-        return True if request.method in SAFE_METHODS else request.user.is_superuser
+        if request.method in SAFE_METHODS:
+            return True
