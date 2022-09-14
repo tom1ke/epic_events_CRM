28f1ce055ed9c1a37dc7da3a7e52f02ba92decee
Thomas Roussel <thomas.roussel@ik.me>
2022-09-14 16:45:06 +0200

    plural name in admin

--- src/status/models.py
+++ src/status/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Status(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'status'
+    
+    
     label = models.CharField(max_length=100)
 
     def __str__(self):
